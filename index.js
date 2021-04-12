34129ee850ba0922f2364f8255c5a5617dea1bb2
josueJURE <josue.jure@gmail.com>
2021-04-12 17:06:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const bottles = document.getElementById("bottles");
+count = 99;
+var myArray = [];
+
+function bottlesOnTheWall() {
+  while (count > 0) {
+    if(count > 2) {
+      myArray.push(`${count} bottles of beer on the wall, ${count} bottles of beers. Take one down and pass it around, ${count - 1} bottles of beer on the wall`)
+    } else if (count === 2) {
+      myArray.push(`${count} bottles of beer on the wall, ${count} bottles of beers. Take one down and pass it around, ${count - 1}bottle of beer on the wall`)
+    } else if (count === 1) {
+      myArray.push(`${count} bottle of beer on the wall, ${count} bottles of beers. No more bottle of beer on the wall`)
+    } else {
+      myArray.push(`No more bottles of beer on the wall. No more bottles of beer.  Go to the shop and buy some ${count} more.`)
+    }
+    count--
+  }
+  console.log(myArray)
+}
+bottlesOnTheWall();
