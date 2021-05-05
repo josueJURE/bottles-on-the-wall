a29622fb3d279da9030f02180b1cd5235ee6f121
josueJURE <josue.jure@gmail.com>
2021-05-05 06:11:07 +0100

    rewrite cleaner if statement

--- index.js
+++ index.js
@@ -2,15 +2,11 @@
 function bottlesOnTheWall(count) {
   while (count > 0) {
     let text;
-    if(count > 2) {
-      text = `${count} bottles of beer on the wall, ${count} bottles of beers. Take one down and pass it around, ${count - 1} bottles of beer on the wall`
-    } else if (count === 2) {
-      text = `${count} bottles of beer on the wall, ${count} bottles of beers. Take one down and pass it around, ${count - 1}bottle of beer on the wall`
-    } else if (count === 1) {
-      text = `${count} bottle of beer on the wall, ${count} bottles of beers. No more bottle of beer on the wall`
-    } else {
-      text = `No more bottles of beer on the wall. No more bottles of beer.  Go to the shop and buy some ${count} more.`
-    }
+    if(count > 2) text = `${count} bottles of beer on the wall, ${count} bottles of beers. Take one down and pass it around, ${count - 1} bottles of beer on the wall`
+     else if (count === 2) text = `${count} bottles of beer on the wall, ${count} bottles of beers. Take one down and pass it around, ${count - 1}bottle of beer on the wall`
+     else if (count === 1) text = `${count} bottle of beer on the wall, ${count} bottles of beers. No more bottle of beer on the wall`
+     else text = `No more bottles of beer on the wall. No more bottles of beer.  Go to the shop and buy some ${count} more.`
+
     const bottles = document.getElementById("bottles");
     const liElement = document.createElement("li");
     liElement.appendChild(document.createTextNode(text))
